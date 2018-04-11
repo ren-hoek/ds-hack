61b893960ace439eae50c3c4c5c8331471503b72
Ren Hoek <ren-hoek@users.noreply.github.com>
2018-04-11 16:44:23 +0000

    added text extractor

--- blob-store/extext.py
+++ blob-store/extext.py
@@ -53,7 +53,6 @@ def process_request( json, data, headers, params ):
         response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params )
 
         if response.status_code == 429:
-            print( "Message: %s" % ( response.json() ) )
             if retries <= _maxNumRetries:
                 time.sleep(1)
                 retries += 1
@@ -65,9 +64,21 @@ def process_request( json, data, headers, params ):
             result = response.headers['Operation-Location']
         else:
             print( "Error code: %d" % ( response.status_code ) )
-            print( "Message: %s" % ( response.json() ) )
         break
     return response.json()
+
+
+def find(key, dictionary):
+    for k, v in dictionary.items():
+        if k == key:
+            yield v
+        elif isinstance(v, dict):
+            for result in find(key, v):
+                yield result
+        elif isinstance(v, list):
+            for d in v:
+                for result in find(key, d):
+                    yield result
 
 
 class AzureBlob():
@@ -84,24 +95,54 @@ class AzureBlob():
 
 
 def main():
-	client = py.MongoClient(os.environ['MONGO_URI'])
-	db = client.ch
+    client = py.MongoClient(os.environ['MONGO_URI'])
+    db = client.ch
 
-	ch = AzureBlob(os.environ['BLOB_NAME'], os.environ['BLOB_KEY'])
-	blob_file = db.docs.find_one({'filetype': 'pdf', 'doctype': 'annual-returns'})
+    ch = AzureBlob(os.environ['BLOB_NAME'], os.environ['BLOB_KEY'])
+    blob_file = db.docs.find_one({'filetype': 'pdf', 'doctype': 'annual-returns'})
 
-	print(blob_file['container'], blob_file['blob_file'])
+    #print(blob_file['container'], blob_file['blob_file'])
 
-	ch.blob.get_blob_to_path(
+    ch.blob.get_blob_to_path(
             blob_file['container'],
             blob_file['blob_file'],
             blob_file['filename']
-		)
+        )
 
-	pages = create_jpg(blob_file['filename'])
-	print(pages['page_count'])
-	print(pages['pages'])
+    pages = create_jpg(blob_file['filename'])
+    #print(pages['page_count'])
+    #print(pages['pages'])
 
+
+
+    params = {'handwriting' : 'true'}
+    headers = dict()
+    headers['Ocp-Apim-Subscription-Key'] = _key
+    headers['Content-Type'] = 'application/octet-stream'
+    json = None
+
+    API_call_flat = []
+
+    for i in pages['pages']:
+        i.save('test.jpg')
+        with open('test.jpg', 'rb') as f:
+            data = f.read()
+        API_call = process_request(json, data, headers, params)
+        API_call_flat.append(list(find('text', API_call)))
+
+
+    extracted_hand = [x for y in API_call_flat for x in y]
+
+    text_output = dict()
+    text_output['extracted_hand'] = extracted_hand
+
+    print(blob_file['filename'])
+
+    for f in db.docs.find({'filename': blob_file['filename']}):
+        db.docs.update_one({'_id': f['_id']}, {'$set': text_output})
+
+    for f in db.docs.find({'filename': blob_file['filename']}):
+        print(f)
 
 if __name__ == "__main__":
     main()
